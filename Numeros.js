93a67269c848f81c94a4f7aeacb4f87cd417d31d
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-04-22 16:03:59 -0400

    se implementaron Objetos y metodos

---
+++ Numeros.js
@@ -0,0 +1,40 @@
+//Declaracion de tipos de datos numericos
+
+let edad= 35 
+console.log(typeof edad) //number
+const gravedad=9.81 
+console.log(typeof gravedad) //number
+
+
+//! Objeto matemático
+
+const PI=Math.PI
+console.log(PI); //3.141592653589793
+console.log(Math.round(PI));
+console.log(Math.round(9.31));
+console.log(Math.floor(PI));
+console.log(Math.ceil(PI));
+console.log(Math.min(-5,3,4,5,10));
+console.log(Math.max(-5,3,4,5,10));
+
+const random= Math.random() //0 0.99999999//
+
+console.log(random);
+
+const numero=Math.floor(Math.random()*11)
+console.log(numero);
+console.log(Math.sqrt(100)) //10
+console.log(Math.log(2)) 
+console.log(Math.log(10)) 
+console.log(Math.LN2) 
+console.log(Math.LN10) 
+console.log(Math.pow(3,2)) //9
+console.log(Math.sin(0));
+console.log(Math.sin(60));
+console.log(Math.cos(0));
+console.log(Math.cos(60));
+
+
+
+
+
---
+++ TiposdeDatos.js
@@ -0,0 +1,59 @@
+//!Los tipos de datos primitivos en JavaScript incluyen:
+
+//? Números: enteros, flotantes
+//?Cadenas //String: cualquier dato entre comillas simples, comillas dobles o comillas invertidas
+//?Booleanos - valor verdadero o falso
+//?Nulo: valor vacío o sin valor
+//?Indefinido - una variable declarada sin un valor
+//?Símbolo: un valor único que puede generar el constructor de símbolos
+
+let palabra= "JavaScript" //String 
+console.log(palabra[5]='Y')
+console.log(palabra)
+//* Comparacion de valores de tipos de datos primitivos
+let numeroUno=3 //number
+let numeroDos=3 //number
+console.log(numeroUno==numeroDos) //true
+
+let js='JavaScript' //String
+let py = 'Python'   //String
+console.log(js==py) //false
+
+let luzEncendida= true
+let luzApagada= false
+console.log(luzEncendida==luzApagada) //false
+//*Los tipos de datos NO primitivos en JavaScript incluyen:
+// Objeto//
+// Arreglos//
+
+//! TIPOS DE DATOS NO PRIMITIVOS
+let numeros=[1,2,3,4]
+console.log(numeros);
+numeros[0]= 10 //[10,2,3,4]
+console.log(numeros);
+let nums=[1,2,3]
+let numeritos=nums
+
+console.log(nums == numeritos) //true
+ let usuario={
+    name:'Susana',
+    reole:'developer',
+    country:1
+ }
+ let copiausuario=usuario
+
+ console.log(copiausuario==usuario)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
